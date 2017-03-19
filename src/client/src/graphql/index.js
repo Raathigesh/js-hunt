d1913f1e1c4939ac31c38f0f7b484dd34cfa390a
ZONE24X7-CMB\raathigeshank <raathigeshank@zone24x7.com>
2017-03-19 18:29:46 +0530

    Prod server

--- src/client/src/graphql/index.js
+++ src/client/src/graphql/index.js
@@ -2,7 +2,7 @@ import { ApolloClient, createNetworkInterface } from "react-apollo";
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: "http://localhost:4000/data"
+    uri: "http://jshunt.azurewebsites.net/data"
   })
 });
 
